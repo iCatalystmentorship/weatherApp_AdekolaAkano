93671f6471ccc13b0e2eb16abfb0c63869200316
Dekola <akano.adekola@gmail.com>
2019-10-10 12:16:55 +0100

    Initial Commit

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.white
     }
 
 
