23bd65881e4cc0b64263f4a82bfd7b622bc8e376
Dekola <akano.adekola@gmail.com>
2019-10-10 11:56:53 +0100

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Dekola Ak on 10/10/2019.
+//  Copyright © 2019 Dekola Ak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
